368ba2b3740c1de0b173c1abfc1309b58ab4a385
anshpatel <ansh@anshrpatel.com>
2024-09-06 00:48:26 +0530

    Changes by devmehta
    added documentation

--- api/models.py
+++ api/models.py
@@ -4,6 +4,17 @@ from django.db import models
 # Create your models here.
 
 class Skill(models.Model):
+    """
+       Model representing a user's skill.
+
+       Fields:
+           name (str): The name of the skill (e.g., 'Python').
+           level (int): Proficiency level of the skill (e.g., 1 for beginner).
+
+       Meta:
+           verbose_name (str): 'Skill'.
+           verbose_name_plural (str): 'Skills'.
+       """
     name = models.CharField(max_length=1024)
     level = models.IntegerField(default=1)
 
@@ -16,6 +27,26 @@ class Skill(models.Model):
 
 
 class Profile(models.Model):
+    """
+       Model representing a user's profile.
+
+       Fields:
+           title (str): A title for the profile (e.g., 'Software Engineer').
+           user (OneToOneField): One-to-one relationship with the User model.
+           bio (str): A short biography or description of the user.
+           location (str): The user's location.
+           birth_date (date): The user's birth date.
+           phone (str): The user's phone number.
+           website (URLField): The user's website URL.
+           image (ImageField): Profile picture.
+           skills (ManyToManyField): Relationship with the Skill model.
+           created_at (datetime): When the profile was created.
+           updated_at (datetime): When the profile was last updated.
+
+       Meta:
+           verbose_name (str): 'Profile'.
+           verbose_name_plural (str): 'Profiles'.
+       """
     title = models.CharField(max_length=1024)
     user = models.OneToOneField('Account.User', on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
@@ -37,6 +68,18 @@ class Profile(models.Model):
 
 
 class Social(models.Model):
+    """
+        Model representing a user's social media link.
+
+        Fields:
+            profile (ForeignKey): Foreign key to the Profile model.
+            name (str): Name of the social platform (e.g., 'LinkedIn').
+            url (URLField): URL of the social profile.
+
+        Meta:
+            verbose_name (str): 'Social'.
+            verbose_name_plural (str): 'Socials'.
+        """
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     name = models.CharField(max_length=1024)
     url = models.URLField()
@@ -50,6 +93,23 @@ class Social(models.Model):
 
 
 class Education(models.Model):
+    """
+        Model representing a user's education.
+
+        Fields:
+            profile (ForeignKey): Foreign key to the Profile model.
+            degree (str): The degree obtained (e.g., 'BSc Computer Science').
+            school (str): Name of the educational institution.
+            field_of_study (str): The user's field of study (e.g., 'Computer Science').
+            start_date (date): Start date of the education.
+            end_date (date, nullable): End date of the education.
+            description (TextField, optional): Description about the education.
+            media (FileField, optional): Optional file (e.g., certificate).
+
+        Meta:
+            verbose_name (str): 'Education'.
+            verbose_name_plural (str): 'Educations'.
+        """
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     degree = models.CharField(max_length=1024)
     school = models.CharField(max_length=1024)
@@ -68,6 +128,23 @@ class Education(models.Model):
 
 
 class Experience(models.Model):
+    """
+       Model representing a user's professional experience.
+
+       Fields:
+           profile (ForeignKey): Foreign key to the Profile model.
+           position (str): The position held (e.g., 'Software Developer').
+           company (str): Name of the company.
+           location (str): Location of the company.
+           start_date (date): Start date of the job.
+           end_date (date, nullable): End date of the job.
+           description (TextField): Description of the role and responsibilities.
+           skills (ManyToManyField): Relationship with the Skill model.
+
+       Meta:
+           verbose_name (str): 'Experience'.
+           verbose_name_plural (str): 'Experiences'.
+       """
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     position = models.CharField(max_length=1024)
     company = models.CharField(max_length=1024)
@@ -86,6 +163,17 @@ class Experience(models.Model):
 
 
 class ExperienceMedia(models.Model):
+    """
+        Model representing media related to a user's experience.
+
+        Fields:
+            experience (ForeignKey): Foreign key to the Experience model.
+            media (FileField): A file associated with the experience.
+
+        Meta:
+            verbose_name (str): 'Experience Media'.
+            verbose_name_plural (str): 'Experience Media'.
+        """
     experience = models.ForeignKey(Experience, on_delete=models.CASCADE)
     media = models.FileField(upload_to='experience_media/')
 
@@ -98,6 +186,23 @@ class ExperienceMedia(models.Model):
 
 
 class Project(models.Model):
+    """
+        Model representing a user's project.
+
+        Fields:
+            profile (ForeignKey): Foreign key to the Profile model.
+            title (str): The title of the project.
+            description (TextField): A brief description of the project.
+            link (URLField, optional): Optional URL linking to the project.
+            start_date (date): Start date of the project.
+            end_date (date, nullable): End date of the project.
+            media (FileField, optional): Media file related to the project.
+            skills (ManyToManyField): Relationship with the Skill model.
+
+        Meta:
+            verbose_name (str): 'Project'.
+            verbose_name_plural (str): 'Projects'.
+        """
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     title = models.CharField(max_length=1024)
     description = models.TextField(max_length=500, blank=True)
@@ -116,6 +221,18 @@ class Project(models.Model):
 
 
 class ProjectMedia(models.Model):
+    """
+        Model representing media files related to a user's project.
+
+        Fields:
+            project (ForeignKey): Foreign key to the Project model.
+            media (FileField): A file associated with the project.
+
+        Meta:
+            verbose_name (str): 'Project Media'.
+            verbose_name_plural (str): 'Project Media'.
+        """
+
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     media = models.FileField(upload_to='project_media/')
 
@@ -128,6 +245,25 @@ class ProjectMedia(models.Model):
 
 
 class Certificate(models.Model):
+    """
+        Model representing a user's certification.
+
+        Fields:
+            profile (ForeignKey): Foreign key to the Profile model.
+            title (str): The name of the certificate.
+            organization (str): Organization issuing the certificate.
+            description (TextField, optional): Optional description of the certificate.
+            link (URLField, optional): Optional link to the certificate.
+            issue_date (date): Date the certificate was issued.
+            expiration_date (date, nullable): Expiry date of the certificate.
+            media (FileField, optional): Certificate file.
+            skills (ManyToManyField): Relationship with the Skill model.
+
+        Meta:
+            verbose_name (str): 'Certificate'.
+            verbose_name_plural (str): 'Certifications'.
+        """
+
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     title = models.CharField(max_length=1024)
     organization = models.CharField(max_length=1024)
@@ -147,6 +283,27 @@ class Certificate(models.Model):
 
 
 class Job(models.Model):
+    """
+        Model representing a job posting.
+
+        Fields:
+            title (str): The title of the job (e.g., 'Software Engineer').
+            company (str): Name of the company offering the job.
+            location (str): Location of the job.
+            type (str): Job type (e.g., internship, part-time, full-time).
+            salary (DecimalField, optional): Optional salary for the job.
+            start_date (date): Start date of the job.
+            end_date (date, nullable): End date of the job.
+            description (TextField): Description of the job role.
+            working_hours (str, optional): Working hours for the job.
+            requirements (TextField): Requirements for the job.
+            responsibilities (TextField): Responsibilities for the job.
+            skills (ManyToManyField): Relationship with the Skill model.
+
+        Meta:
+            verbose_name (str): 'Job'.
+            verbose_name_plural (str): 'Jobs'.
+        """
     title = models.CharField(max_length=1024, verbose_name="Job Title/Position")
     company = models.CharField(max_length=1024)
     location = models.CharField(max_length=1024)
@@ -177,6 +334,18 @@ class Job(models.Model):
 
 
 class JobSkill(models.Model):
+    """
+        Model representing a specific skill required for a job.
+
+        Fields:
+            job (ForeignKey): Foreign key to the Job model.
+            name (str): Name of the skill.
+            level (int): Required proficiency level for the skill.
+
+        Meta:
+            verbose_name (str): 'Job Skill'.
+            verbose_name_plural (str): 'Job Skills'.
+        """
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
     name = models.CharField(max_length=1024)
     level = models.IntegerField(default=1)
@@ -190,6 +359,17 @@ class JobSkill(models.Model):
 
 
 class JobMedia(models.Model):
+    """
+        Model representing media related to a job posting.
+
+        Fields:
+            job (ForeignKey): Foreign key to the Job model.
+            media (FileField): Media file related to the job.
+
+        Meta:
+            verbose_name (str): 'Job Media'.
+            verbose_name_plural (str): 'Job Media'.
+        """
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
     media = models.FileField(upload_to='job_media/')
 
@@ -202,6 +382,21 @@ class JobMedia(models.Model):
 
 
 class JobListing(models.Model):
+    """
+        Model representing a job listing.
+
+        Fields:
+            job (ForeignKey): Foreign key to the Job model.
+            employer (ForeignKey): Profile of the employer.
+            recruiter (ForeignKey, optional): Profile of the recruiter.
+            status (str): Status of the job (e.g., upcoming, active, closed).
+            start_date (date): Start date of the job listing.
+            deadline (date): Application deadline for the job.
+
+        Meta:
+            verbose_name (str): 'Job Listing'.
+            verbose_name_plural (str): 'Job Listings'.
+        """
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
     employer = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='employer')
     recruiter = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='recruiter', blank=True, null=True)
@@ -222,6 +417,20 @@ class JobListing(models.Model):
 
 
 class JobApplication(models.Model):
+    """
+        Model representing a user's job application.
+
+        Fields:
+            job (ForeignKey): Foreign key to the Job model.
+            applicant (ForeignKey): Profile of the applicant.
+            cover_letter (TextField, optional): Optional cover letter for the application.
+            status (str): Status of the application (e.g., pending, accepted, rejected).
+            custom_status (str, optional): Custom status for the application.
+
+        Meta:
+            verbose_name (str): 'Job Application'.
+            verbose_name_plural (str): 'Job Applications'.
+        """
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
     applicant = models.ForeignKey(Profile, on_delete=models.CASCADE)
     cover_letter = models.TextField(max_length=500, blank=True, null=True)
@@ -241,6 +450,25 @@ class JobApplication(models.Model):
 
 
 class Course(models.Model):
+    """
+       Model representing a course taken by a user.
+
+       Fields:
+           profile (ForeignKey): Foreign key to the Profile model.
+           title (str): Title of the course.
+           organization (str): Organization offering the course.
+           description (TextField): Description of the course.
+           link (URLField, optional): Optional URL to the course.
+           start_date (date): Start date of the course.
+           end_date (date, nullable): End date of the course.
+           certificate (FileField, optional): File of the certificate obtained from the course.
+           verification_status (str): Status of the course (e.g., pending, verified, rejected).
+           skills (ManyToManyField): Relationship with the Skill model.
+
+       Meta:
+           verbose_name (str): 'Course'.
+           verbose_name_plural (str): 'Courses'.
+       """
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     title = models.CharField(max_length=1024)
     organization = models.CharField(max_length=1024)
@@ -265,6 +493,24 @@ class Course(models.Model):
 
 
 class MentorStatusRequest(models.Model):
+    """
+        Model to store the status of a mentor request.
+
+        Attributes:
+            mentor (ForeignKey): A reference to the Profile of the mentor.
+            status (CharField): The current status of the request (Pending, Accepted, Rejected). Defaults to 'pending'.
+            message (TextField): Optional message from the mentee when requesting mentorship.
+            response (TextField): Optional response from the mentor regarding the request.
+            created_at (DateTimeField): Timestamp when the request was created. Automatically set at creation.
+            updated_at (DateTimeField): Timestamp when the request was last updated. Automatically set at each update.
+
+        Meta:
+            verbose_name (str): Human-readable singular name for the model.
+            verbose_name_plural (str): Human-readable plural name for the model.
+
+        Methods:
+            __str__(): Returns the mentor's username as a string representation of the object.
+        """
     mentor = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='mentor')
     status = models.CharField(max_length=1024, choices=(
         ('pending', 'Pending'),
@@ -285,6 +531,25 @@ class MentorStatusRequest(models.Model):
 
 
 class MentorshipRequest(models.Model):
+    """
+        Model to store requests for mentorship between a mentor and a mentee.
+
+        Attributes:
+            mentor (ForeignKey): A reference to the mentor's Profile.
+            mentee (ForeignKey): A reference to the mentee's Profile.
+            status (CharField): The current status of the mentorship request (Pending, Accepted, Rejected). Defaults to 'pending'.
+            message (TextField): Optional message from the mentee.
+            response (TextField): Optional response from the mentor.
+            created_at (DateTimeField): Timestamp when the request was created.
+            updated_at (DateTimeField): Timestamp when the request was last updated.
+
+        Meta:
+            verbose_name (str): Human-readable singular name for the model.
+            verbose_name_plural (str): Human-readable plural name for the model.
+
+        Methods:
+            __str__(): Returns the mentor's username as a string representation of the object.
+    """
     mentor = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='mentor_request')
     mentee = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='mentee_request')
     status = models.CharField(max_length=1024, choices=(
@@ -306,6 +571,25 @@ class MentorshipRequest(models.Model):
 
 
 class Endorsement(models.Model):
+    """
+        Model to store endorsements given by users to each other for specific skills.
+
+        Attributes:
+            endorser (ForeignKey): The user providing the endorsement.
+            endorsee (ForeignKey): The user receiving the endorsement.
+            skill (ManyToManyField): The skill(s) for which the endorsement is made.
+            message (TextField): Optional message for the endorsement.
+            created_at (DateTimeField): Timestamp when the endorsement was created.
+            updated_at (DateTimeField): Timestamp when the endorsement was last updated.
+
+        Meta:
+            verbose_name (str): Human-readable singular name for the model.
+            verbose_name_plural (str): Human-readable plural name for the model.
+
+        Methods:
+            __str__(): Returns the endorser's username as a string representation of the object.
+        """
+
     endorser = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='endorser')
     endorsee = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='endorsee')
     skill = models.ManyToManyField(Skill, blank=True)
@@ -322,6 +606,25 @@ class Endorsement(models.Model):
 
 
 class MentorshipWork(models.Model):
+    """
+        Model to store the work done between a mentor and a mentee.
+
+        Attributes:
+            mentor (ForeignKey): A reference to the mentor's Profile.
+            mentee (ForeignKey): A reference to the mentee's Profile.
+            title (CharField): Title of the mentorship work.
+            description (TextField): A brief description of the work.
+            start_date (DateField): Date when the work started.
+            end_date (DateField): Date when the work ended (optional).
+            media (FileField): Optional media file associated with the work.
+
+        Meta:
+            verbose_name (str): Human-readable singular name for the model.
+            verbose_name_plural (str): Human-readable plural name for the model.
+
+        Methods:
+            __str__(): Returns the title of the mentorship work as a string representation of the object.
+        """
     mentor = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='mentor_work')
     mentee = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='mentee_work')
     title = models.CharField(max_length=1024)
@@ -339,6 +642,27 @@ class MentorshipWork(models.Model):
 
 
 class FeaturedCourse(models.Model):
+    """
+        Model to store featured courses on the platform.
+
+        Attributes:
+            course (ForeignKey): A reference to the Course model.
+            description (TextField): Brief description of the course.
+            price (DecimalField): Price of the course.
+            duration (CharField): Duration of the course.
+            level (CharField): The course level (Beginner, Intermediate, Advanced).
+            link (URLField): Optional URL link to the course.
+            media (FileField): Optional media associated with the course.
+            created_at (DateTimeField): Timestamp when the course was featured.
+            updated_at (DateTimeField): Timestamp when the course information was last updated.
+
+        Meta:
+            verbose_name (str): Human-readable singular name for the model.
+            verbose_name_plural (str): Human-readable plural name for the model.
+
+        Methods:
+            __str__(): Returns the course title as a string representation of the object.
+        """
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     description = models.TextField(max_length=500, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
